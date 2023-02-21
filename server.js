7db35bbae03a4391ec121cdf493ba5321ab88bbd
Skusmirek <skusmirek@student.cvtc.edu>
2023-02-20 22:07:30 -0600

    First commit

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const newFolderName = function (folders) {
 
+  let counter = 1
+  let folderName = 'New Folder '
+  for (let i = 0; i < folders.length; i++ ) {
+    counter++
+  }
+
+  let message = folderName + "" +  counter; 
+
+  return message
+
 }
+
+console.log(newFolderName(['New Folder']))
+console.log(newFolderName(['New Folder', 'New Folder (2)', 'New Folder (3)']))
